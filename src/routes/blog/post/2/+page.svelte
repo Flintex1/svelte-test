a431069efcf97940391b81cd5a13d72084a97a19
Flintex <flintex1@gmail.com>
2023-05-29 21:53:12 +0700

    add blog

---
+++ src/routes/blog/post/2/+page.svelte
@@ -0,0 +1,3 @@
+<h1>My Second Blog Post</h1>
+
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum id veniam nesciunt totam blanditiis nihil perferendis voluptate. Molestiae doloremque ex, quo magnam ipsum dolorum similique delectus amet omnis unde consectetur.</p>
---
+++ src/routes/blog/routes.js
@@ -0,0 +1,17 @@
+export const routes = [
+    {
+        id: 1,
+        title: 'My first blog post',
+        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus illo ullam magnam iure. Tenetur ad unde rem consequatur iste, repellat minima, ea, voluptates cupiditate voluptas pariatur quas magnam asperiores earum.'
+    },
+    {
+        id: 2,
+        title: 'My second blog post',
+        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus illo ullam magnam iure. Tenetur ad unde rem consequatur iste, repellat minima, ea, voluptates cupiditate voluptas pariatur quas magnam asperiores earum.'
+    },
+    {
+        id: 3,
+        title: 'My third blog post',
+        body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus illo ullam magnam iure. Tenetur ad unde rem consequatur iste, repellat minima, ea, voluptates cupiditate voluptas pariatur quas magnam asperiores earum.'
+    }
+];
